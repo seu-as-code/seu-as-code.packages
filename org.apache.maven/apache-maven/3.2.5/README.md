0847399c593885f44fc7d8c3a0a2d79079a7876e
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2015-07-30 21:52:22 +0200

    Added Apache Maven packages.

---
+++ org.apache.maven/apache-maven/3.2.5/README.md
@@ -0,0 +1,8 @@
+# Apache Maven 3.0.5
+
+The package contains the Apache Maven 3.0.5 release. Install this SEU-as-Code package using the following dependency:
+```groovy
+dependencies {
+	software 'org.apache.maven:apache-maven:3.0.5'
+}
+```
---
+++ org.apache.maven/apache-maven/3.3.3/files/set-env-apache-maven.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+set MAVEN_HOME=%SOFTWARE_DIR%\apache-maven-3.3.3
+set MAVEN_OPTS=-Xmx4096M -Xms512M -XX:MaxPermSize=512M -Dfile.encoding=UTF-8
+
+set M3_HOME=%MAVEN_HOME%
+set M3_REPO=%SEU_HOME%\repository
+
+set PATH=%M3_HOME%\bin;%PATH%
