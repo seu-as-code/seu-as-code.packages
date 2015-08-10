d5095d9d3ebce7301ec9e5c6b20cd3c3ddbeacc3
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2015-08-10 23:48:03 +0200

    Fixed short cut hook.

--- org.apache.lucene/solr/5.2.1/files/META-INF/hooks/createSolr5Shortcut.groovy
+++ org.apache.lucene/solr/5.2.1/files/META-INF/hooks/createSolr5Shortcut.groovy
@@ -4,12 +4,12 @@ Scriptom.inApartment
 {
 	def wshShell = new ActiveXObject("WScript.Shell")
     def start = wshShell.CreateShortcut("$seuHome\\Start-Solr5.lnk")
-    start.TargetPath = "$seuHome\\software\\start-solr5.bat"	
-    start.WorkingDirectory = "$seuHome\\software"
+    start.TargetPath = "${seuLayout.software}\\start-solr5.bat"	
+    start.WorkingDirectory = "${seuLayout.software}"
 	start.Save()
 	
 	def stop = wshShell.CreateShortcut("$seuHome\\Stop-Solr5.lnk")
-    stop.TargetPath = "$seuHome\\software\\stop-solr5.bat"	
-    stop.WorkingDirectory = "$seuHome\\software"
+    stop.TargetPath = "${seuLayout.software}\\stop-solr5.bat"	
+    stop.WorkingDirectory = "${seuLayout.software}"
 	stop.Save()
 }
