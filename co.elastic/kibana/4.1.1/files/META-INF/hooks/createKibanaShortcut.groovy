4907c206113734837f9527f23065ac8482f0360d
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2015-08-18 23:40:17 +0200

    Used new hook conventions.

--- co.elastic/kibana/4.1.1/files/META-INF/hooks/createKibanaShortcut.groovy
+++ co.elastic/kibana/4.1.1/files/META-INF/hooks/createKibanaShortcut.groovy
@@ -4,7 +4,7 @@ import org.codehaus.groovy.scriptom.Scriptom
 Scriptom.inApartment {
     def wshShell = new ActiveXObject("WScript.Shell")
     def shortcut = wshShell.CreateShortcut("$seuHome\\kibana.lnk")
-    shortcut.TargetPath = "$seuHome\\software\\kibana-4.1.1-windows\\bin\\kibana.bat"
-    shortcut.WorkingDirectory = "$seuHome\\software"
+    shortcut.TargetPath = "${seuLayout.software}\\kibana-4.1.1-windows\\bin\\kibana.bat"
+    shortcut.WorkingDirectory = "${seuLayout.software}"
     shortcut.Save()
 }
