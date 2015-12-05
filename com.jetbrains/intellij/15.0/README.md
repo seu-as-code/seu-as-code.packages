656c16e5e06dcbf4fe5237035f7607c749d756d6
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2015-12-05 21:33:39 +0100

    Fine tuning and adjustments to IntelliJ 15 package.

--- com.jetbrains/intellij/15.0/README.md
+++ com.jetbrains/intellij/15.0/README.md
@@ -1,4 +1,4 @@
-# IntelliJ Ultimate 14.1.4
+# IntelliJ Ultimate 15.0
 
 The package contains the IntelliJ Ultimate 15.0 release. Install this SEU-as-Code package using the following dependency:
 ```groovy
--- com.jetbrains/intellij/15.0/files/META-INF/hooks/createIntellij15Shortcut.groovy
+++ com.jetbrains/intellij/15.0/files/META-INF/hooks/createIntellij15Shortcut.groovy
@@ -5,8 +5,8 @@ Scriptom.inApartment
 {
 	def wshShell = new ActiveXObject("WScript.Shell")
 	def shortcut = wshShell.CreateShortcut("$seuHome\\intellij15.lnk")
-	shortcut.TargetPath = "$seuHome\\software\\start-intellij-15.0.bat"
-	shortcut.IconLocation = "$seuHome\\software\\intellij-15.0\\bin\\idea64.exe, 0"
+	shortcut.TargetPath = "${seuLayout.software}\\start-intellij-15.0.bat"
+	shortcut.IconLocation = "${seuLayout.software}\\intellij-15.0\\bin\\idea64.exe, 0"
 	shortcut.WorkingDirectory = "$seuHome\\software"
 	shortcut.Save()
 }
