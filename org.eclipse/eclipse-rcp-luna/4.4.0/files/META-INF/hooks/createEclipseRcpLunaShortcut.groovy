5123388284d2901e46648e4c5c28874454203a37
Christian Fritz <christian.fritz@qaware.de>
2016-05-24 17:54:34 +0200

    Fixes the short cut path.

--- org.eclipse/eclipse-rcp-luna/4.4.0/files/META-INF/hooks/createEclipseRcpLunaShortcut.groovy
+++ org.eclipse/eclipse-rcp-luna/4.4.0/files/META-INF/hooks/createEclipseRcpLunaShortcut.groovy
@@ -4,7 +4,7 @@ import org.codehaus.groovy.scriptom.Scriptom
 Scriptom.inApartment
 {
 	def wshShell = new ActiveXObject("WScript.Shell")
-	def shortcut = wshShell.CreateShortcut("$seuHome\\eclipse.lnk")
+	def shortcut = wshShell.CreateShortcut("$seuHome\\eclipse-rcp-luna.lnk")
 	shortcut.TargetPath = "${seuLayout.software}\\start-eclipse-rcp-luna.bat"
 	shortcut.IconLocation = "${seuLayout.software}\\eclipse-rcp-luna\\eclipse.exe, 0"
 	shortcut.WorkingDirectory = "${seuLayout.software}"
