8bac91451aba55b7f1e326b90097e91e89afc7df
Christian Fritz <christian.fritz@qaware.de>
2016-05-24 17:02:00 +0200

    Adds new package for Eclipse RCP Luna (4.4)

---
+++ org.eclipse/eclipse-rcp-luna/4.4.0/README.md
@@ -0,0 +1,8 @@
+# Eclipse for RCP Developer (LUNA 4.4)
+
+The package contains the Eclipse for RCP Developer release in version 4.4.0. Install this SEU-as-Code package using the following dependency:
+```groovy
+dependencies {
+	software 'org.eclipse:eclipse-rcp-luna:4.4.0'
+}
+```
---
+++ org.eclipse/eclipse-rcp-luna/4.4.0/files/META-INF/hooks/createEclipseRcpLunaShortcut.groovy
@@ -0,0 +1,12 @@
+import org.codehaus.groovy.scriptom.ActiveXObject
+import org.codehaus.groovy.scriptom.Scriptom
+
+Scriptom.inApartment
+{
+	def wshShell = new ActiveXObject("WScript.Shell")
+	def shortcut = wshShell.CreateShortcut("$seuHome\\eclipse.lnk")
+	shortcut.TargetPath = "${seuLayout.software}\\start-eclipse-rcp-luna.bat"
+	shortcut.IconLocation = "${seuLayout.software}\\eclipse-rcp-luna\\eclipse.exe, 0"
+	shortcut.WorkingDirectory = "${seuLayout.software}"
+	shortcut.Save()
+}
