bc389a9f495da7934df208e9527e01d58e537474
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-07-08 13:59:59 +0200

    Finalized Gradle 2.14 package.

--- org.gradle/gradle/2.14/files/set-env-gradle.cmd
+++ org.gradle/gradle/2.14/files/set-env-gradle.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set GRADLE_HOME=%SOFTWARE_DIR%\gradle-2.13
+set GRADLE_HOME=%SOFTWARE_DIR%\gradle-2.14
 set GRADLE_OPTS=-Xmx4096M -Xms512M -Dfile.encoding=UTF-8
 
 set PATH=%GRADLE_HOME%\bin;%PATH%
