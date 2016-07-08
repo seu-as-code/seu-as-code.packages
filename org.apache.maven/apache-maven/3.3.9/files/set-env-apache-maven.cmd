c29e32d195b3ef27f032b5c15f3cc50f51f5d5c9
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-07-08 10:18:36 +0200

    Added new package for Apache Maven 3.3.9.

--- org.apache.maven/apache-maven/3.3.9/files/set-env-apache-maven.cmd
+++ org.apache.maven/apache-maven/3.3.9/files/set-env-apache-maven.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-set MAVEN_HOME=%SOFTWARE_DIR%\apache-maven-3.3.3
+set MAVEN_HOME=%SOFTWARE_DIR%\apache-maven-3.3.9
+
 set MAVEN_OPTS=-Xmx4096M -Xms512M -XX:MaxPermSize=512M -Dfile.encoding=UTF-8
 
 set M3_HOME=%MAVEN_HOME%
---
+++ org.apache.maven/apache-maven/3.3.9/files/set-env-apache-maven.sh
@@ -0,0 +1,7 @@
+export MAVEN_HOME=$SOFTWARE_DIR/apache-maven-3.3.9
+
+export MAVEN_OPTS="-Xmx4096M -Xms512M -XX:MaxPermSize=512M -Dfile.encoding=UTF-8"
+
+export M3_HOME=$MAVEN_HOME
+
+export PATH=$MAVEN_HOME/bin:$PATH
