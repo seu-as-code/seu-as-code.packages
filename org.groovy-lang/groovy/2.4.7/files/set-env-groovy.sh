1813f41e6a1974635a9f743e27175ae12b162210
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-09-06 00:31:49 +0200

    Fix for Groovy package env script. Closes no issue.

--- org.groovy-lang/groovy/2.4.7/files/set-env-groovy.sh
+++ org.groovy-lang/groovy/2.4.7/files/set-env-groovy.sh
@@ -1,4 +1,4 @@
 
 export GROOVY_HOME=$SOFTWARE_DIR/groovy-2.4.7
-export JAVA_OPTS=$JAVA_OPTS -Dgrape.config=$JAVA_OPTS/conf/grapeConfig.xml -Dgroovy.root=$JAVA_OPTS/.groovy/
+export JAVA_OPTS="$JAVA_OPTS -Dgrape.config=$GROOVY_HOME/conf/grapeConfig.xml -Dgroovy.root=$GROOVY_HOME/.groovy/"
 export PATH=$GROOVY_HOME/bin:$PATH
