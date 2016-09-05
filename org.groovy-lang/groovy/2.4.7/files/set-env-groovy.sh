69feb5b8b45c934319c3e2421d7e8db6e77b0451
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-09-05 23:33:25 +0200

    Fixed Groovy package shell scripts.

---
+++ org.groovy-lang/groovy/2.4.7/files/set-env-groovy.sh
@@ -0,0 +1,4 @@
+
+export GROOVY_HOME=$SOFTWARE_DIR/groovy-2.4.7
+export JAVA_OPTS=$JAVA_OPTS -Dgrape.config=$JAVA_OPTS/conf/grapeConfig.xml -Dgroovy.root=$JAVA_OPTS/.groovy/
+export PATH=$GROOVY_HOME/bin:$PATH
