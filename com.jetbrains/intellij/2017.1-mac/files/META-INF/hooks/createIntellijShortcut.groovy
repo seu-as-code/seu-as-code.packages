93d5c1ad78cdc57a8f5ff6a487c8bec0f9a31f1a
Christian Fritz <christian.fritz@qaware.de>
2017-03-24 22:49:33 +0100

    Corrects mount of dmg image. (Manual unmount after building the package needed)

--- com.jetbrains/intellij/2017.1-mac/files/META-INF/hooks/createIntellijShortcut.groovy
+++ com.jetbrains/intellij/2017.1-mac/files/META-INF/hooks/createIntellijShortcut.groovy
@@ -3,5 +3,5 @@ import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.attribute.PosixFilePermissions
 
-Files.createSymbolicLink(Paths.get("${seuLayout.software}/IntelliJ IDEA.app"), 
-	Paths.get("${seuHome}/IntelliJ IDEA.app"), PosixFilePermissions.fromString("rwxrwxrwx"))
+Files.createSymbolicLink(Paths.get("${seuHome}/IntelliJ IDEA.app"),
+	Paths.get("${seuLayout.software}/IntelliJ IDEA.app"))
