f5923ce802a5a15e3cb15b1e84aa6dd050a39a22
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2017-07-08 17:38:48 +0200

    Updated package to Gradle 4.0.1

--- org.gradle/gradle/4.x/files/set-env-gradle.sh
+++ org.gradle/gradle/4.x/files/set-env-gradle.sh
@@ -1,5 +1,5 @@
 
-export GRADLE_HOME=$SOFTWARE_DIR/gradle-4.0
+export GRADLE_HOME=$SOFTWARE_DIR/gradle-4.0.1
 export GRADLE_OPTS="-Xmx4096M -Xms512M -Dfile.encoding=UTF-8"
 
 export PATH=$GRADLE_HOME/bin:$PATH
