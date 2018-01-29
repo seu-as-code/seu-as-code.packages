f1803c6dc9dbae7539879ce0f5e6d06555cd0beb
Christian Fritz <christian.fritz@qaware.de>
2018-01-29 15:46:26 +0100

    Added latest version 180114 of conemu.
    - Prefixed group name of package with "seuac." to avoid coordination conflicts with java maven packages.

---
+++ io.github.conemu/conemu/180114/files/META-INF/hooks/createConEmuShortcut.groovy
@@ -0,0 +1,11 @@
+import org.codehaus.groovy.scriptom.*
+
+Scriptom.inApartment
+{
+	def wshShell = new ActiveXObject("WScript.Shell")
+    def shortcut = wshShell.CreateShortcut("$seuHome\\ConEmu.lnk")
+    shortcut.TargetPath = "${seuLayout.software}\\start-conemu.bat"
+    shortcut.IconLocation = "${seuLayout.software}\\ConEmuPack.180114\\ConEmu64.exe"
+    shortcut.WorkingDirectory = "${seuLayout.software}"
+	shortcut.Save()
+}
