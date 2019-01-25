11e2d442a57066ff50692df76ce45706033b5622
Stephan Klapproth <stephan.klapproth@qaware.de>
2019-01-25 16:58:34 +0100

    New package for NodeJS version 10.15.0 (current lts version)

---
+++ org.nodejs/node/10.x/files/mac/set-env-node.sh
@@ -0,0 +1,4 @@
+
+export NODE_HOME=$SOFTWARE_DIR/node-v10.15.0-darwin-x64
+
+export PATH=$NODE_HOME/bin:$PATH
---
+++ org.nodejs/node/10.x/files/unix/set-env-node.sh
@@ -0,0 +1,4 @@
+
+export NODE_HOME=$SOFTWARE_DIR/node-v10.15.0-linux-x64
+
+export PATH=$NODE_HOME/bin:$PATH
